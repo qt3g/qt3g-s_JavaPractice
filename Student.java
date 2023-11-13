cba8c32930767fadde96c9d31b093908997e2392
qt3g <140607840+qt3g@users.noreply.github.com>
2023-11-13 09:37:24 +0900

    Update Student.java

--- Student.java
+++ Student.java
@@ -4,7 +4,7 @@ public class Student {
 	String name;
 	String id;
 	
-	public Student(String X,String Y) {
+	public Student(String X,String Y) {//コンストラクタの定義　クラス名と同じ名前で定義する
 		this.name = X;
 		this.id = Y;
 	}
