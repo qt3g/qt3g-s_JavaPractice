34091d047568fa9a7bc05d3b57b9f8101359dacf
qt3g <140607840+qt3g@users.noreply.github.com>
2023-11-13 11:06:56 +0900

    Update Main.java

--- no5/Main.java
+++ no5/Main.java
@@ -2,15 +2,15 @@ package no5;
 
 class Main {
 	 public static void main(String[] args) {
-		 Hero h = new Hero();
+		 Hero h = new Hero();//	インスタンスの生成
 		 h.name = "ミナト";
 		 h.hp = 100;
 	
-		 Matango m1 = new Matango();
+		 Matango m1 = new Matango();//	インスタンスの生成
 		 m1.hp = 50;
 		 m1.suffix = 'A';
 	
-		 Matango m2 = new Matango();
+		 Matango m2 = new Matango();//	インスタンスの生成
 		 m2.hp = 48;
 		 m2.suffix = 'B';
 	
