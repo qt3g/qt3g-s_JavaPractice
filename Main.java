4d1ef2e020dd2f57ab96d76b86353e077138993e
qt3g <140607840+qt3g@users.noreply.github.com>
2023-11-13 09:33:34 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package no6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Student x;
+		
+		x = new Student("日工太郎","121I999");
+		
+		System.out.println(x.name + "さんのIDは" + x.id + "です");
+	}
+
+}
