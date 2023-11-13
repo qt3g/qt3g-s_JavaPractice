4f539a4d1ced885293061dd1cae1364f536d3eb5
qt3g <140607840+qt3g@users.noreply.github.com>
2023-11-13 11:14:37 +0900

    Update Student.java

--- no6/Student.java
+++ no6/Student.java
@@ -6,7 +6,7 @@ public class Student {
 
 	//コンストラクタの定義
 	//クラス名前と同名のメソッドとして定義する
-	public Student(String X,String Y) {//原則一つのコンストラクタには、名前以上の機能はつけてはいけない
+	public Student(String X,String Y) {//原則一つのコンストラクタには、名前以上の機能はつけてはいけない（理論上可能だけど、スパゲティ化の原因）
 		this.name = X;
 		this.id = Y;
 	}
